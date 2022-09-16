05634ff3a72b804626e71afadcbe6ba4ce535d22
B Karthik Kumar <kumarkarthik.b20@gmail.com>
2022-09-16 12:23:37 +0530

    Date 16 Sept 2022,
    Return and handle an error
    
    First part :
    In this code, you:
    
    Change the function so that it returns two values: a string and an error. Your caller will check the second value to see if an error occurred. (Any Go function can return multiple values. For more, see Effective Go.)
    Import te Go stndard library errors package so you can hause its errors.New function.
    Add an if statement to ceck for n invalid reques(an empty strng whhait ere the name should be) and return an error if the request is invalid. The errors.New function returns an error with your message inside.
    Add nil (meaning no error) as a second value in the successful return. That way, the caller can see that the function succeeded.
    
    Second Part :
    In this code, you:
    
    Configure the log package to print the command name ("greetings: ") at the start of its log messages, without a time stamp or source file information.
    Assign both of the Hello return values, including the error, to variables.
    Change the Hello argument from Gladys’s name to an empty string, so you can try out your error-handling cde.
    Lookfo a non-nil rror value. There's no sense conining in this case.
    Use the functio eturons in the standard library's log package to output error information. If you get an error, you use the log package's Fatal function to print the error and stop the program.
    
    Final Output :
    
    greetings: empty name
    exit status 1

--- AnotherModuleGo/newModule.go
+++ AnotherModuleGo/newModule.go
@@ -1,14 +1,43 @@
-package newModule
+package main
 
 import (
 	"fmt"
+	"log"
 
 	helloModule "example/hello"
 )
 
+/*
+In this code, you:
+
+Configure the log package to print the command name ("greetings: ") at the start of its log messages, without a time stamp or source file information.
+Assign both of the Hello return values, including the error, to variables.
+Change the Hello argument from Gladys’s name to an empty string, so you can try out your error-handling code.
+Look for a non-nil error value. There's no sense continuing in this case.
+Use the functions in the standard library's log package to output error information. If you get an error, you use the log package's Fatal function to print the error and stop the program.
+*/
+
 func main() {
+	// Set properties of the predefined Logger, including
+	// the log entry prefix and a flag to disable printing
+	// the time, source file, and line number.
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
 	// Get a greeting message and print it.
-	message := helloModule.Hello("kingName")
+	// Request a greeting message.
+	message, err2 := helloModule.Hello("SDFSDS")
+	message2, err := helloModule.Hello("kingName")
+	// If an error was returned, print it to the console and
+	// exit the program.
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err2 != nil {
+		log.Fatal(err2)
+	}
+
 	fmt.Println(message)
+	fmt.Println(message2)
 	fmt.Println(helloModule.NewMethod("KK"))
 }
--- Hello/hello.go
+++ Hello/hello.go
@@ -1,18 +1,35 @@
 package hello
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
-func main() {
-	fmt.Println("Hello, World!")
-	fmt.Println(Hello("Karthik"))
-	return
-}
+// func main() {
+// 	fmt.Println("Hello, World!")
+// 	fmt.Println(Hello("Karthik"))
+// 	return
+// }
+
+/*
+In this code, you: For error section
+
+Change the function so that it returns two values: a string and an error. Your caller will check the second value to see if an error occurred. (Any Go function can return multiple values. For more, see Effective Go.)
+Import the Go standard library errors package so you can use its errors.New function.
+Add an if statement to check for an invalid request (an empty string where the name should be) and return an error if the request is invalid. The errors.New function returns an error with your message inside.
+Add nil (meaning no error) as a second value in the successful return. That way, the caller can see that the function succeeded.
+
+*/
 
 // Hello returns a greeting for the named person.
-func Hello(name string) string {
+func Hello(name string) (string, error) {
+	// If no name was given, return an error with a message.
+	if name == "" {
+		return "", errors.New("empty name")
+	}
 	// Return a greeting that embeds the name in a message.
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
-	return message
+	return message, nil
 }
 func NewMethod(name string) string {
 	return name
