c26d6e0dab5116407a21cfdead5404d494bf73f3
B Karthik Kumar <kumarkarthik.b20@gmail.com>
2022-09-16 20:19:36 +0530

    Addedd Slice for the following : In this code, you:
    Add a Hellos function whose parameter is a slice of names rather than a single name. Also, you change one of its return types from a string to a map so you can return names mapped to greeting messages.
    Have the new Hellos function call the existing Hello function. This helps reduce duplication while also leaving both functions in place.
    Create a messages map to associate each of the received names (as a key) with a generated message (as a value). In Go, you initialize a map with the following syntax: make(map[key-type]value-type). You have the Hellos function return this map to the caller. For more about maps, see Go maps in action on the Go blog.
    Loop through the names your function received, checking that each has a non-empty value, then associate a message with each. In this for loop, range returns two values: the index of the current item in the loop and a copy of the item's value. You don't need the index, so you use the Go blank identifier (an underscore) to ignore it. For more, see The blank identifier in Effective Go.

--- Hello/hello.go
+++ Hello/hello.go
@@ -33,6 +33,25 @@ func Hello(name string) (string, error) {
 	message := fmt.Sprintf(randomFormat(), name)
 	return message, nil
 }
+
+// Hellos returns a map that associates each of the named people
+// with a greeting message.
+func Hellos(names []string) (map[string]string, error) {
+	// A map to associate names with messages.
+	messages := make(map[string]string)
+	// Loop through the received slice of names, calling
+	// the Hello function to get a message for each name.
+	for _, name := range names {
+		message, err := Hello(name)
+		if err != nil {
+			return nil, err
+		}
+		// In the map, associate the retrieved message with
+		// the name.
+		messages[name] = message
+	}
+	return messages, nil
+}
 func NewMethod(name string) string {
 	return name
 } // init sets initial values for variables used in the function.
@@ -66,5 +85,17 @@ Use the math/rand package to generate a random number for selecting an item from
 Add an init function to seed the rand package with the current time. Go executes init functions automatically at program startup, after global variables have been initialized. For more about init functions, see Effective Go.
 In Hello, call the randomFormat function to get a format for the message you'll return, then use the format and name value together to create the message.
 Return the message (or an error) as you did before.
+
+*/
+
+/*
+Slice
+In this code, you:
+
+Add a Hellos function whose parameter is a slice of names rather than a single name. Also, you change one of its return types from a string to a map so you can return names mapped to greeting messages.
+Have the new Hellos function call the existing Hello function. This helps reduce duplication while also leaving both functions in place.
+Create a messages map to associate each of the received names (as a key) with a generated message (as a value). In Go, you initialize a map with the following syntax: make(map[key-type]value-type). You have the Hellos function return this map to the caller. For more about maps, see Go maps in action on the Go blog.
+Loop through the names your function received, checking that each has a non-empty value, then associate a message with each. In this for loop, range returns two values: the index of the current item in the loop and a copy of the item's value. You don't need the index, so you use the Go blank identifier (an underscore) to ignore it. For more, see The blank identifier in Effective Go.
+
 
 */
